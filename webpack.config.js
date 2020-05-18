aa92dae6479161de7435a0a26c1bed4436c3eeb6
muskuloes <t.victor@campus.lmu.de>
2020-05-18 23:25:24 +0200

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlwebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlwebpackPlugin({
+      template: "index.html",
+    }),
+  ],
+};
